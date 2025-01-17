d954278da6bf71c7be86ecd5ca6bf05ac75b4864
Muhamad Ali Ridho <muhamadaliridho01@gmail.com>
2025-01-17 14:31:17 +0700

    meta piksel Fb

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,29 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
        <head>
+      {/* Facebook Pixel Script */}
+      <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '977374340971729');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=977374340971729&ev=PageView&noscript=1"
+          />
+        </noscript>
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8454337513998618"
