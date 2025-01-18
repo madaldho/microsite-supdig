665dc407d84bef3877dc846faf640b8cacf3f678
Muhamad Ali Ridho <muhamadaliridho01@gmail.com>
2025-01-18 11:41:15 +0700

    add gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,16 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KJ654KV9');
+  `}
+        </Script>
+
         {/* adnsene */}
         <meta name="google-adsense-account" content="ca-pub-8454337513998618" />
         {/* Google Search Console Verification */}
@@ -50,7 +60,18 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           crossOrigin="anonymous"
           strategy="afterInteractive" // Strategi loading untuk script
         />
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          {/* Google Tag Manager (noscript) */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-KJ654KV9"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}></iframe>
+          </noscript>
+
+          {children}
+        </body>
       </head>
     </html>
   );
