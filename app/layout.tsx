0970d2f3b693f8091e277c2a7bac7843d886a580
Muhamad Ali Ridho <muhamadaliridho01@gmail.com>
2025-01-18 12:36:54 +0700

    add analitic

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,16 +13,6 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
-        <Script id="gtm-script" strategy="afterInteractive">
-          {`
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-KJ654KV9');
-  `}
-        </Script>
-
         {/* adnsene */}
         <meta name="google-adsense-account" content="ca-pub-8454337513998618" />
         {/* Google Search Console Verification */}
@@ -54,6 +44,29 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             src="https://www.facebook.com/tr?id=977374340971729&ev=PageView&noscript=1"
           />
         </noscript>
+        {/* Meta Tags gtm google */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KJ654KV9');
+  `}
+        </Script>
+
+        {/* Google tag (gtag.js) */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-HP58H3LY8Q"></Script>
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-HP58H3LY8Q');
+  `}
+        </Script>
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8454337513998618"
